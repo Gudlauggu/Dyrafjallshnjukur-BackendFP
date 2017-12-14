cd5837182ab78775ea803c009be7c41cb23bcc2b
ingvarorn87 <ingvarorn87@gmail.com>
2017-12-14 12:39:22 +0100

    Set up structure for Jwt Security

---
+++ DemoDAL/Helpers/UserValidator.cs
@@ -0,0 +1,51 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAL.Helpers
+{
+    class UserValidator
+    {
+        
+        User _user;
+        public UserValidator(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentException("User cannot be null");
+            }
+            if (user.Username == null)
+            {
+                throw new ArgumentException("Username cannot be null");
+            }
+            _user = user;
+        }
+
+        public void ValidateUserName()
+        {
+            if (_user.Username.Length < 6)
+            {
+                throw new ArgumentException("Username to Short");
+            }
+            if (_user.Username.Length > 20)
+            {
+                throw new ArgumentException("Username to Long");
+            }
+            if (Regex.IsMatch(_user.Username, @"(.)\1\1"))
+            {
+                throw new ArgumentException("Username Can not contain same char 3 or more times in a row");
+            }
+            if (_user.Username.Contains(",") ||
+               _user.Username.Contains(".") ||
+               _user.Username.Contains("(") ||
+               _user.Username.Contains(")") ||
+               _user.Username.Contains("/") ||
+               _user.Username.Contains("_"))
+            {
+                throw new ArgumentException("Username Cannot contain Special Chars like '.', ',', '/', '_', '(' and ')'");
+            }
+        }
+    }
+}
