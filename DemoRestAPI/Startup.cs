64cf71409bb60da12eb8a169a92fbfe531fd566f
Gudlauggu <gudl0025@easv365.dk>
2017-12-20 00:52:22 +0100

    startup error because of extra line

--- DemoRestAPI/Startup.cs
+++ DemoRestAPI/Startup.cs
@@ -8,9 +8,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using Swashbuckle.AspNetCore.Swagger;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using AppRestAPI.Helpers;
 
 namespace CustomerRestAPI
 {
@@ -36,21 +33,6 @@ namespace CustomerRestAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add JWT based authentication
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateAudience = false,
-                    //ValidAudience = "TodoApiClient",
-                    ValidateIssuer = false,
-                    //ValidIssuer = "TodoApi",
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = JwtSecurityKey.Key,
-                    ValidateLifetime = true, //validate the expiration and not before values in the token
-                    ClockSkew = TimeSpan.FromMinutes(5) //5 minute tolerance for the expiration date
-                };
-            });
             services.AddMvc();
 
             // Register the Swagger generator, defining one or more Swagger documents
@@ -78,79 +60,72 @@ namespace CustomerRestAPI
                 app.UseDeveloperExceptionPage();
                 var facade = new BLLFacade();
 
-                //facade.UserService.Create(new UserBO() { Username = "Admin1", Password = "1234", Role = "Administrator" });
-                //facade.UserService.Create(new UserBO() { Username = "Admin2", Password = "1234", Role = "Administrator" });
-                //facade.UserService.Create(new UserBO() { Username = "User1", Password = "1234", Role = "User" });
-                //facade.UserService.Create(new UserBO() { Username = "User2", Password = "1234", Role = "User" });
+                var user = facade.UserService.Create(
+                    new UserBO()
+                    {
+                        Password = "hoho",
+                        Role = "Admin",
+                        Username = "Moana"
 
-                //var user = facade.UserService.Create(
-                //    new UserBO()
-                //    {
-                //        Password = "hoho",
-                //        Role = "Admin",
-                //        Salt = "121221",
-                //        Username = "Moana"
+                    });
 
-                //    });
+                var profile = facade.ProfileService.Create(
+                    new ProfileBO()
+                    {
+                        FirstName = "Sir Ian",
+                        LastName = "McKellen",
+                        Address = "Isengard",
+                        Email = "TheOneTrueWizard@Hobbiton.me",
+                        PhoneNumber = "22221177"
+                    }
+                    );
 
-                //var profile = facade.ProfileService.Create(
-                //    new ProfileBO()
-                //    {
-                //        FirstName = "Sir Ian",
-                //        LastName = "McKellen",
-                //        Address = "Isengard",
-                //        Email = "TheOneTrueWizard@Hobbiton.me",
-                //        PhoneNumber = "22221177"
-
-                //    }
-                //    );
-
-                //var itemType = facade.ItemTypeService.Create(
-                //   new ItemTypeBO()
-                //   {
-                //       Name = "Cyder"
-                //   }
-                //   );
-                //var item1 = facade.ItemService.Create(
-                //    new ItemBO()
-                //    {
-                //        Name = "Somersby",
-                //        ItemTypeId = itemType.Id
-                //    });
-                //var item2 = facade.ItemService.Create(
-                //    new ItemBO()
-                //    {
-                //        Name = "Breezer",
-                //        ItemTypeId = itemType.Id
-                //    });
-                //var item3 = facade.ItemService.Create(
-                //    new ItemBO()
-                //    {
-                //        Name = "Mokai",
-                //        ItemTypeId = itemType.Id
-                //    });
+                var itemType = facade.ItemTypeService.Create(
+                   new ItemTypeBO()
+                   {
+                       Name = "Cyder"
+                   }
+                   );
+                var item1 = facade.ItemService.Create(
+                    new ItemBO()
+                    {
+                        Name = "Somersby",
+                        ItemTypeId = itemType.Id
+                    });
+                var item2 = facade.ItemService.Create(
+                    new ItemBO()
+                    {
+                        Name = "Breezer",
+                        ItemTypeId = itemType.Id
+                    });
+                var item3 = facade.ItemService.Create(
+                    new ItemBO()
+                    {
+                        Name = "Mokai",
+                        ItemTypeId = itemType.Id
+                    });
 
 
-                //var pub = facade.PubService.Create(
-                //    new PubBO()
-                //    {
-                //        Name = "Esbjerg City",
-                //        Address = "Bobparkerway 52",
-                //        ItemIds = new List<int>() { item1.Id, item2.Id, item3.Id }
-                //    }
-                //    );
+                var pub = facade.PubService.Create(
+                    new PubBO()
+                    {
+                        Name = "Esbjerg City",
+                        Address = "Bobparkerway 52",
+                        ItemIds = new List<int>() { item1.Id, item2.Id, item3.Id }
+                    }
+                    );
 
-                //var order = facade.OrderService.Create(
-                //    new OrderBO()
-                //    {
-                //        OrderDate = DateTime.Now,
-                //        DeliveryDate = DateTime.Now.AddDays(30),
-                //        OrderPrice = 10000000,
-                //        Supplier = "SUPPLIER!",
-                //        PubId = pub.Id,
-                //        ItemIds = new List<int>() { item1.Id, item2.Id, item3.Id }
+                var order = facade.OrderService.Create(
+                    new OrderBO()
+                    {
+                        OrderDate = DateTime.Now,
+                        DeliveryDate = DateTime.Now.AddDays(30),
+                        OrderPrice = 10000000,
+                        Supplier = "SUPPLIER!",
+                        PubId = pub.Id,
+                        ItemIds = new List<int>() { item1.Id, item2.Id, item3.Id }
 
-                //    });
+                    });
 
 
 
